32ed84364a594d5efae7b1d77fd167194d3eb330
dudapierri <pierriduda@users.noreply.github.com>
2024-05-13 10:28:35 -0300

    Adicionei novas configurações

---
+++ aula14ex/ex017/script.js
@@ -0,0 +1,17 @@
+function gerar() {
+    let num = document.getElementById('inumero')
+    let tab = document.getElementById('seltab')
+    if(num.value.length == 0) {
+        window.alert('Por favor digite um número')
+    } else {
+        let n = Number(num.value)
+        let c = 1
+        while(c <= 10) {
+            let item = document.createElement('option')
+            item.text = `${n} x ${c} = ${n*c}`
+            tab.appendChild(item)
+            c++
+        }
+    }
+    
+} 
